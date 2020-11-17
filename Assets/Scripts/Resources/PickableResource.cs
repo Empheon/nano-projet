945d0ba749b0808a6cd209ae555f276875674b85
TheoLemoine <theo.lemoine0@gmail.com>
2020-11-17 14:52:21 +0100

    reworked interactions, added resources

--- Assets/Scripts/Resources/PickableResource.cs
+++ Assets/Scripts/Resources/PickableResource.cs
@@ -1,19 +1,40 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using Character;
 
 namespace Resources
 {
     public class PickableResource : MonoBehaviour
     {
+        [SerializeField] private string duringInteractionTag = "Untagged";
+        [SerializeField] private ResourceTypes type = ResourceTypes.None;
 
-        [SerializeField] private ResourceTypes type;
+        private string _baseTag;
         
+        private Rigidbody2D _rb;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+            _baseTag = gameObject.tag;
+        }
+
         private void OnCharacterInteract(GameObject character)
         {
             var resource = new Resource(type, gameObject);
             var characterResource = character.GetComponent<CharacterResource>();
-            
+
+            _rb.simulated = false;
+            gameObject.tag = duringInteractionTag;
+
             characterResource.StoreResource(resource);
+        }
+
+        private void OnLetResourceDown(Vector2 baseVelocity)
+        {
+            _rb.simulated = true;
+            _rb.velocity = baseVelocity;
+            gameObject.tag = _baseTag;
         }
         
     }
