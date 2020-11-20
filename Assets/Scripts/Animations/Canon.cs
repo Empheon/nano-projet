4024bb9cd574a67f96cec36e99366589c99a7431
Thomas Foucault <t.foucault2@gmail.com>
2020-11-20 15:31:47 +0100

    start anim scripts

---
+++ Assets/Scripts/Animations/Canon.cs
@@ -0,0 +1,44 @@
+﻿using PathCreation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Animations
+{
+    public class Canon : MonoBehaviour
+    {
+        [SerializeField]
+        private Rocket rocketPrefab;
+
+        [SerializeField]
+        private PathCreator pathCreatorAtt;
+        [SerializeField]
+        private PathCreator pathCreatorDef;
+        [SerializeField]
+        private PathCreator pathCreatorJam;
+
+        private const string ATTACK_KEY = "attack";
+        private const string DEFENCE_KEY = "defence";
+        private const string JAMMING_KEY = "jam";
+
+        public void Shoot(string pathKey)
+        {
+            Rocket rocket = Instantiate(rocketPrefab);
+            switch (pathKey)
+            {
+                case ATTACK_KEY:
+                    rocket.Init(pathCreatorAtt);
+                    break;
+                case DEFENCE_KEY:
+                    rocket.Init(pathCreatorDef);
+                    break;
+                case JAMMING_KEY:
+                    rocket.Init(pathCreatorJam);
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Animations/Rocket.cs
@@ -0,0 +1,42 @@
+﻿using PathCreation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using DG.Tweening;
+
+namespace Animations
+{
+    public class Rocket : MonoBehaviour
+    {
+        [SerializeField]
+        private float duration;
+        [SerializeField]
+        private Ease easing = Ease.Linear;
+
+        private PathCreator m_pathCreator;
+        private float m_distanceTravelled;
+
+        public void Init(PathCreator pathCreator)
+        {
+            m_pathCreator = pathCreator;
+            DOTween.To(() => m_distanceTravelled, (x) => m_distanceTravelled = x, 1, duration).SetEase(easing);
+            UpdatePositionAndRotation();
+        }
+
+        private void Update()
+        {
+            if (m_pathCreator == null) return;
+
+            UpdatePositionAndRotation();
+        }
+
+        private void UpdatePositionAndRotation()
+        {
+            transform.position = m_pathCreator.path.GetPointAtDistance(m_distanceTravelled, EndOfPathInstruction.Stop);
+            transform.rotation = m_pathCreator.path.GetRotationAtDistance(m_distanceTravelled, EndOfPathInstruction.Stop);
+        }
+    }
+}
