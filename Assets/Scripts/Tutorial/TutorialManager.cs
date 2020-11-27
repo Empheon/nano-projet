6a454a7d38ba4a08e8e0380f785dbdcd47a8de73
Thomas Foucault <t.foucault2@gmail.com>
2020-11-27 21:39:55 +0100

    start tuto

---
+++ Assets/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+namespace Tutorial
+{
+    public class TutorialManager : MonoBehaviour
+    {
+        [SerializeField]
+        private MechaLights leftMechaLights;
+        [SerializeField]
+        private MechaLights rightMechaLights;
+
+
+    }
+
+    [Serializable]
+    public struct MechaLights
+    {
+        public Light2D AttLight;
+        public Light2D DefLight;
+        public Light2D JamLight;
+        public Light2D MunLight;
+        public Light2D EneLight;
+        public Light2D FixLight;
+    }
+}
---
+++ Assets/Scripts/UI/UIMenuManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class UIMenuManager : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject titleWrapper;
+
+        public void OnClickPlay()
+        {
+            SceneManager.LoadScene(3);
+        }
+
+        public void OnClickQuit()
+        {
+            Application.Quit();
+        }
+    }
+}
