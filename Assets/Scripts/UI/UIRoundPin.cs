17b695fd65a56ec6944811886827211cceb63760
Thomas Foucault <t.foucault2@gmail.com>
2020-11-28 19:24:54 +0100

    Fix UI

--- Assets/Scripts/UI/UIRoundPin.cs
+++ Assets/Scripts/UI/UIRoundPin.cs
@@ -4,17 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 namespace UI
 {
     public class UIRoundPin : MonoBehaviour
     {
         [SerializeField]
-        private GameObject fillerObject;
+        private Sprite fillerSprite;
 
-        public void DisplayFiller(bool b)
+        public void DisplayFiller()
         {
-            fillerObject.SetActive(b);
+            Sequence seq = DOTween.Sequence();
+            seq.Append(transform.DOScale(1.2f, 0.5f).SetEase(Ease.OutCirc));
+            seq.Append(transform.DOScale(1, 0.5f).SetEase(Ease.InCirc));
+
+            GetComponent<Image>().DOColor(new Color(195 / 255f, 50 / 255f, 50 / 255f), 0.5f);
         }
     }
 }
