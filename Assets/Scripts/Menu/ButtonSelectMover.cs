7a9adc00047616bccbcbe0156be1624c2584b462
Thomas Foucault <t.foucault2@gmail.com>
2020-12-12 18:40:21 +0100

    update menu

---
+++ Assets/Scripts/Menu/ButtonSelectMover.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using DG.Tweening;
+
+namespace Menu
+{
+    public class ButtonSelectMover : MonoBehaviour
+    {
+        private Animator m_animator;
+
+        private void Start()
+        {
+            m_animator = GetComponent<Animator>();
+        }
+
+        public void OnSelect()
+        {
+            m_animator.SetBool("Selected", true);
+        }
+
+        public void OnDeselect()
+        {
+            m_animator.SetBool("Selected", false);
+        }
+    }
+}
